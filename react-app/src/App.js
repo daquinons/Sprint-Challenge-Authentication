4b46ae7186e59b049636a4b4d77b4a0bdbd19ff9
David Quiñones <daquinons@users.noreply.github.com>
2019-07-26 11:18:09 +0200

    Add React app

---
+++ react-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Index from './components/Index';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Signout from './components/Signout';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <Route path="/" exact component={Index} />
+      <Route path="/signout" component={Signout} />
+      <Route path="/signup/" component={Signup} />
+      <Route path="/signin/" component={Signin} />
+    </div>
+  );
+}
+
+export default App;
