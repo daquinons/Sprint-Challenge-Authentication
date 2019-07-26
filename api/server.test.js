c5187b85d020cd25667b68bbc0f3cbadd1db40f5
David Quiñones <daquinons@users.noreply.github.com>
2019-07-26 11:22:58 +0200

    Remove unused variable

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,6 @@
 const server = require('./server');
 const request = require('supertest');
 const db = require('../database/dbConfig');
-const Users = require('../database/userModel');
 
 beforeEach(async () => {
   await db('users').truncate();
